afb3ea380fbcb412d339c4f6756bea2666157331
Jim Brännlund <jimbrannlund@fastmail.com>
2019-08-11 17:50:00 +0200

    Use pre-commit in project

--- pytest_base_url/plugin.py
+++ pytest_base_url/plugin.py
@@ -10,52 +10,53 @@ from requests.packages.urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def base_url(request):
     """Return a base URL"""
     config = request.config
-    base_url = config.getoption('base_url')
+    base_url = config.getoption("base_url")
     if base_url is not None:
         return base_url
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def _verify_url(request, base_url):
     """Verifies the base URL"""
     verify = request.config.option.verify_base_url
     if base_url and verify:
         session = requests.Session()
-        retries = Retry(backoff_factor=0.1,
-                        status_forcelist=[500, 502, 503, 504])
+        retries = Retry(backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
         session.mount(base_url, HTTPAdapter(max_retries=retries))
         session.get(base_url)
 
 
 def pytest_configure(config):
-    if hasattr(config, 'slaveinput'):
+    if hasattr(config, "slaveinput"):
         return  # xdist slave
-    base_url = config.getoption('base_url') or config.getini('base_url')
+    base_url = config.getoption("base_url") or config.getini("base_url")
     if base_url is not None:
         config.option.base_url = base_url
-        if hasattr(config, '_metadata'):
-            config._metadata['Base URL'] = base_url
+        if hasattr(config, "_metadata"):
+            config._metadata["Base URL"] = base_url
 
 
 def pytest_report_header(config, startdir):
-    base_url = config.getoption('base_url')
+    base_url = config.getoption("base_url")
     if base_url:
-        return 'baseurl: {0}'.format(base_url)
+        return "baseurl: {0}".format(base_url)
 
 
 def pytest_addoption(parser):
-    parser.addini('base_url', help='base url for the application under test.')
+    parser.addini("base_url", help="base url for the application under test.")
     parser.addoption(
-        '--base-url',
-        metavar='url',
-        default=os.getenv('PYTEST_BASE_URL', None),
-        help='base url for the application under test.')
+        "--base-url",
+        metavar="url",
+        default=os.getenv("PYTEST_BASE_URL", None),
+        help="base url for the application under test.",
+    )
     parser.addoption(
-        '--verify-base-url',
-        action='store_true',
-        default=not os.getenv('VERIFY_BASE_URL', 'false').lower() == 'false',
-        help='verify the base url.')
+        "--verify-base-url",
+        action="store_true",
+        default=not os.getenv("VERIFY_BASE_URL", "false").lower() == "false",
+        help="verify the base url.",
+    )
--- tests/test_base_url.py
+++ tests/test_base_url.py
@@ -4,65 +4,81 @@
 
 
 def test_fixture(testdir):
-    testdir.makepyfile("""
+    testdir.makepyfile(
+        """
         import pytest
         @pytest.fixture(scope='session')
         def base_url():
             return 'foo'
         def test_fixture(base_url):
             assert base_url == 'foo'
-    """)
+    """
+    )
     result = testdir.runpytest()
     assert result.ret == 0
 
 
 def test_cli(testdir):
-    testdir.makepyfile("""
+    testdir.makepyfile(
+        """
         def test_funcarg(base_url):
             assert base_url == 'foo'
-    """)
-    result = testdir.runpytest('--base-url', 'foo')
+    """
+    )
+    result = testdir.runpytest("--base-url", "foo")
     assert result.ret == 0
 
 
 def test_config(testdir):
-    testdir.makefile('.ini', pytest="""
+    testdir.makefile(
+        ".ini",
+        pytest="""
         [pytest]
         base_url=foo
-    """)
-    testdir.makepyfile("""
+    """,
+    )
+    testdir.makepyfile(
+        """
         def test_config(request, base_url):
             assert request.config.getvalue('base_url') == 'foo'
             assert request.config.getini('base_url') == 'foo'
             assert base_url == 'foo'
-    """)
+    """
+    )
     result = testdir.runpytest()
     assert result.ret == 0
 
 
 def test_skip_config(testdir):
-    testdir.makefile('.ini', pytest="""
+    testdir.makefile(
+        ".ini",
+        pytest="""
         [pytest]
         base_url=foo
-    """)
-    testdir.makepyfile("""
+    """,
+    )
+    testdir.makepyfile(
+        """
         import pytest
         @pytest.mark.skipif(
-            pytest.config.getoption('base_url') == 'foo',
+            "config.getoption('base_url') == 'foo'",
             reason='skip')
         def test_skip_config(): pass
-    """)
+    """
+    )
     result = testdir.runpytest()
     assert result.ret == 0
 
 
 def test_env_var_set(testdir, monkeypatch):
-    testdir.makepyfile("""
+    testdir.makepyfile(
+        """
         def test_config(request, base_url):
             assert request.config.getvalue('base_url')
             assert base_url == 'yeehaw'
-    """)
-    monkeypatch.setenv('PYTEST_BASE_URL', 'yeehaw')
+    """
+    )
+    monkeypatch.setenv("PYTEST_BASE_URL", "yeehaw")
     reprec = testdir.inline_run()
     passed, skipped, failed = reprec.listoutcomes()
     assert len(passed) == 1
